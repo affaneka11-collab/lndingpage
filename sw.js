bc9f0223e37ad51526f03531fe1ef012f40879b2
Affan Eka <affaneka11@gmail.com>
2026-02-22 00:14:35 +0700

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,72 @@
+const CACHE_NAME = 'my-site-cache-v1';
+const urlsToCache = [
+    '/',
+    '/index.html',
+    '/offline.html',
+    '/manifest.json',
+    '/assets/css/style.css',
+    '/assets/js/main.js'
+];
+
+// Install Service Worker
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                console.log('Opened cache');
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+// Activate Service Worker
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (cacheName !== CACHE_NAME) {
+                        console.log('Deleting old cache:', cacheName);
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
+
+// Fetch event - serve from cache or network
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                // Return cached version or fetch from network
+                if (response) {
+                    return response;
+                }
+                
+                return fetch(event.request).then(response => {
+                    // Don't cache non-successful responses
+                    if (!response || response.status !== 200 || response.type !== 'basic') {
+                        return response;
+                    }
+                    
+                    // Clone the response
+                    const responseToCache = response.clone();
+                    
+                    caches.open(CACHE_NAME)
+                        .then(cache => {
+                            cache.put(event.request, responseToCache);
+                        });
+                    
+                    return response;
+                });
+            })
+            .catch(() => {
+                // Return offline page for navigation requests
+                if (event.request.mode === 'navigate') {
+                    return caches.match('/offline.html');
+                }
+            })
+    );
+});
